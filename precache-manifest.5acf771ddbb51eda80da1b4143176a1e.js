e0eb73706c262c44e494176dfe3350e5551ebd50
Alexandra <alexbushueva27@gmail.com>
2020-09-10 21:35:31 +0300

    Updates

---
+++ precache-manifest.5acf771ddbb51eda80da1b4143176a1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdaf30eb2b528559aebb381f05027884",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "4e723a05939eb413b4e1",
+    "url": "/Counter/static/css/main.4a281539.chunk.css"
+  },
+  {
+    "revision": "b05d33cd17c2f5b14e4d",
+    "url": "/Counter/static/js/2.f8a5f653.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter/static/js/2.f8a5f653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e723a05939eb413b4e1",
+    "url": "/Counter/static/js/main.57c75422.chunk.js"
+  },
+  {
+    "revision": "4a2bea4ebda10b11d1d6",
+    "url": "/Counter/static/js/runtime-main.a4bce0b8.js"
+  }
+]);
